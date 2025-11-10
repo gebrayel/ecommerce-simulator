cd7db39525956bcd93f15b70a5d561b7c498a9b2
Gebrayel Inatti <gebrayelinatti06@gmail.com>
2025-11-10 18:50:51 -0400

    first commit

---
+++ src/main/java/com/example/ecommerce_simulator/EcommerceSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSimulatorApplication.class, args);
+	}
+
+}
